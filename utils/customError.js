cf8ea68f688dae3c7dfb87d31e51b166801ef834
mayuresh-byte <mayureshpitambare@gmail.com>
2024-09-09 23:40:16 +0530

    Feat: small improvements in customError class

--- utils/customError.js
+++ utils/customError.js
@@ -1,7 +1,19 @@
 class CustomError extends Error {
-    constructor(message,code){
+    constructor(message, code, details = {}) {
         super(message)
-        this.code = code
+
+        this.code = code;
+        this.details = details;
+
+        // we'll be excluding all the noise data from stack trace
+        if(Error.captureStackTrace) {
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+
+    // For debugging purpose
+    logError() {
+        console.error(`Error: ${this.message}, Code: ${this.code}, Details: ${JSON.stringify(this.details)}`);
     }
 }
 
